6125020229dfc9460afdf5bcde0fd26910e7d33c
ja5m1n3 <100544334+ja5m1n3@users.noreply.github.com>
2022-04-05 12:42:05 +1200

    Add files via upload

---
+++ 00_sandbox.py
@@ -0,0 +1,8 @@
+# Ask users for number
+get_number = int(input("Choose a number?"))
+
+# Multiply the number by 5 
+times_five = get_number * 5
+
+# output the resultd 
+print("{} x 5 = {}".format(get_number, times_five))
