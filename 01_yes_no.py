57fca33fa2516207a742719862a6a431042d71fc
ja5m1n3 <100544334+ja5m1n3@users.noreply.github.com>
2022-04-08 09:32:04 +1200

    Add files via upload

--- 01_yes_no.py
+++ 01_yes_no.py
@@ -1,22 +1,20 @@
-# Ask the user if they have played before 
-show_instructions = input ("Have you played this game before? ") .lower()
 
-# If they day yes, output 'program continues' 
-if show_instructions == "yes": 
-    print ("program continues")
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+    # Ask the user if they have played before 
+    show_instructions = input ("Have you played this game before? ") .lower()
 
-elif show_instructions == "y":
-    print("program continues")
+    # If they day yes, output 'program continues' 
+    if show_instructions == "yes" or show_instructions == "y":
+        show_instructions = "yes"
+        print ("program continues")
 
-elif show_instructions ==  "no":
-    print ("program continues")
+    elif show_instructions ==  "no" or show_instructions == "n":
+        print ("display instructions")
 
-elif show_instructions == "n":
-    print ("program continues")
-
-# If they say no, output 'display instructions'
-else :
-    print ("Please answer yes / no")
+    # If they say no, output 'display instructions'
+    else :
+        print ("Please answer yes / no")
 
     # print("Display instructions")
-    
+        
